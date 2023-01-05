fb4624eb15dd32f2e507bcc621665ce4191c8f74
Filipe <filipeandrade000@gmail.com>
2023-01-05 10:04:42 -0300

    Excecoes para editar pessoa

--- src/main/java/com/attornatus/cadastropessoa/services/PessoaService.java
+++ src/main/java/com/attornatus/cadastropessoa/services/PessoaService.java
@@ -10,29 +10,35 @@ import com.attornatus.cadastropessoa.entities.Pessoa;
 import com.attornatus.cadastropessoa.repositories.PessoaRepository;
 import com.attornatus.cadastropessoa.services.exception.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class PessoaService {
 
 	@Autowired
 	private PessoaRepository repository;
-	
-	public List<Pessoa> listarPessoas(){
+
+	public List<Pessoa> listarPessoas() {
 		return repository.findAll();
 	}
-	
+
 	public Pessoa consultarPessoa(Long id) {
 		Optional<Pessoa> obj = repository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
-	
+
 	public Pessoa criarPessoa(Pessoa obj) {
 		return repository.save(obj);
 	}
-	
+
 	public Pessoa editarPessoa(Long id, Pessoa obj) {
-		Pessoa entity = repository.getReferenceById(id);
-		entity.setNome(obj.getNome());
-		entity.setDataNascimento(obj.getDataNascimento());
-		return repository.save(entity);
+		try {
+			Pessoa entity = repository.getReferenceById(id);
+			entity.setNome(obj.getNome());
+			entity.setDataNascimento(obj.getDataNascimento());
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 }
