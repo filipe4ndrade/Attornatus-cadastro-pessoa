27eaff51ac46d2a95f2fd5691474f7a060880692
Filipe <filipeandrade000@gmail.com>
2023-01-05 06:53:30 -0300

    Primeiro Commit

---
+++ src/main/java/com/attornatus/cadastropessoa/CadastropessoaApplication.java
@@ -0,0 +1,13 @@
+package com.attornatus.cadastropessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastropessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastropessoaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/attornatus/cadastropessoa/CadastropessoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.cadastropessoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastropessoaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
