be0c2ba08a62cf6610acdfac79ed31312314b301
Filipe <filipeandrade000@gmail.com>
2023-01-05 08:14:14 -0300

    H2 database, test profile, JPA

--- src/main/java/com/attornatus/cadastropessoa/entities/Pessoa.java
+++ src/main/java/com/attornatus/cadastropessoa/entities/Pessoa.java
@@ -4,11 +4,25 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_pessoa")
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 	private Date dataNascimento;
 
 	public Pessoa() {
